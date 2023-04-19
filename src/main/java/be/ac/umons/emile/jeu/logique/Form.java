cdc3a0a15b57879628dac2b021345540326aba64
altaicaAV <colinwillems99@gmail.com>
2023-04-19 10:12:53 +0200

    début forme

--- src/main/java/be/ac/umons/emile/jeu/logique/Form.java
+++ src/main/java/be/ac/umons/emile/jeu/logique/Form.java
@@ -11,8 +11,180 @@ public class Form {
 
 
         if(value==1){
-            form.add(new PlayableCases(0,0,value));
+            for(int i = 1; i < 6; i++){
+                for(int j = 1; j < 6; j++){
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+
+        }
+        if(value == 2){
+            for(int i = 1; i < 5; i++){
+                for(int j = 1; j < 5; j++){
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for(PlayableCases p:form) {
+                if (p.getX() == 1 && p.getY() == 4)
+                    p.value = 0;
+                if (p.getX() == 2 && p.getY() == 2)
+                    p.value = 0;
+            }
+        }
+
+        if(value == 3){
+            for(int i = 1; i < 7; i++){
+                for(int j = 1; j < 7; j++){
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for(PlayableCases p:form) {
+                if (p.getX() == 1 && p.getY() == 1)
+                    p.value = 0;
+                if (p.getX() == 1 && p.getY() == 2)
+                    p.value = 0;
+                if (p.getX() == 6 && p.getY() == 5)
+                    p.value = 0;
+                if (p.getX() == 6 && p.getY() == 6)
+                    p.value = 0;
+            }
+        }
+        if(value == 4){
+            for(int i = 1; i < 7; i++){
+                for(int j = 1; j < 7; j++){
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+
+        }
+        if(value == 5){
+            for(int i = 1; i < 8; i++){
+                for(int j = 1; j < 8; j++){
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+
+        }
+        if(value == 6){
+            for(int i = 1; i < 9; i++){
+                for(int j = 1; j < 9; j++){
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+
+        }
+        if(value == 7) {
+            for (int i = 1; i < 7; i++) {
+                for (int j = 1; j < 7; j++) {
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for (PlayableCases p : form) {
+                if (p.getX() == 6 && p.getY() == 1)
+                    p.value = 0;
+            }
+        }
+        if(value == 8){
+            for(int i = 1; i < 8; i++){
+                for(int j = 1; j < 8; j++){
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for (PlayableCases p : form) {
+                if (p.getX() == 1 && p.getY() == 6)
+                    p.value = 0;
+                if (p.getX() == 4 && p.getY() == 1)
+                    p.value = 0;
+            }
+        }
+        if(value == 9) {
+            for (int i = 1; i < 7; i++) {
+                for (int j = 1; j < 7; j++) {
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for (PlayableCases p : form) {
+                if (p.getX() == 1 && p.getY() == 6)
+                    p.value = 0;
+            }
+        }
+        if(value == 10) {
+            for (int i = 1; i < 6; i++) {
+                for (int j = 1; j < 6; j++) {
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for (PlayableCases p : form) {
+                if (p.getX() == 3 && p.getY() == 1)
+                    p.value = 0;
+            }
+        }
+        if(value == 11) {
+            for (int i = 1; i < 6; i++) {
+                for (int j = 1; j < 6; j++) {
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for (PlayableCases p : form) {
+                if (p.getX() == 1 && p.getY() == 1)
+                    p.value = 0;
+                if (p.getX() == 5 && p.getY() == 1)
+                    p.value = 0;
+                if (p.getX() == 3 && p.getY() == 2)
+                    p.value = 0;
+            }
+        }
+        if(value == 12) {
+            for (int i = 1; i < 6; i++) {
+                for (int j = 1; j < 7; j++) {
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for (PlayableCases p : form) {
+                if (p.getX() == 1 && p.getY() == 1)
+                    p.value = 0;
+                if (p.getX() == 5 && p.getY() == 1)
+                    p.value = 0;
+            }
+        }
+        if(value == 13) {
+            for (int i = 1; i < 8; i++) {
+                for (int j = 1; j < 8; j++) {
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for (PlayableCases p : form) {
+                if (p.getX() == 4 && p.getY() == 1)
+                    p.value = 0;
+            }
+        }
+        if(value == 14) {
+            for (int i = 1; i < 8; i++) {
+                for (int j = 1; j < 8; j++) {
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for (PlayableCases p : form) {
+                if (p.getX() == p.getY())
+                    p.value = 0;
+            }
+        }
+        if(value == 15) {
+            for (int i = 1; i < 7; i++) {
+                for (int j = 1; j < 7; j++) {
+                    form.add(new PlayableCases(i, j, value));
+                }
+            }
+            for (PlayableCases p : form) {
+                if (p.getX() == 1 && p.getY() == 1)
+                    p.value = 0;
+                if (p.getX() == 2 && p.getY() == 1)
+                    p.value = 0;
+                if (p.getX() == 6 && p.getY() == 5)
+                    p.value = 0;
+                if (p.getX() == 6 && p.getY() == 6)
+                    p.value = 0;
+            }
         }
     }
-
 }
--- src/main/java/be/ac/umons/emile/jeu/logique/Test.java
+++ src/main/java/be/ac/umons/emile/jeu/logique/Test.java
@@ -3,20 +3,22 @@ package be.ac.umons.emile.jeu.logique;
 import java.util.ArrayList;
 public class Test {
     public static void main(String args[]){
-        Pieces piece= new Pieces(30);
-        piece.printPiece();
-
-
-
-
-
-        /*
-        for(int i=0;i<12;i++){
-            System.out.print(piece.shape[i].getX());
+        int value = 1;
+        ArrayList<PlayableCases> form= new ArrayList<>();
+        for(int i = 1; i < 5; i++){
+            for(int j = 1; j < 5; j++){
+                form.add(new PlayableCases(i, j, value));
+            }
         }
-
-         */
-
+        for(PlayableCases p:form){
+            if (p.getX()==1 && p.getY()== 4)
+                p.value= 0;
+        }
+        for (PlayableCases c:form){
+            System.out.print(c.value);
+        }
     }
+
+
 }
 
